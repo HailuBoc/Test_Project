06a63acb525459b76bf30f0a1880189aec228813
HailuBoc <hailegebrelyalemberabate@outlook.com>
2022-10-19 09:33:00 +0300

    Update styles - 2022-01-17 09:54

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 // Commit 33 - 2022-04-11 15:50:53
+// Commit 33 - 2022-01-17 09:54:02
